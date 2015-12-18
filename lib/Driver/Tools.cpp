4298e31d6bde496e4545c840064ef49a190fe2cb
Jack Chung <jack@multicorewareinc.com>
2015-12-18 16:59:04 +0800

    [Driver] hot-fix to pass -fPIC to hc-host-assemble
    In this commit we pass -fPIC to hc-host-assemble if it's available in the input
    
    This could allow host objects in HC mode be position indepdent. More proper fix
    are still needed to pass all required comple-time flags to hc-host-assemble.

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -2648,6 +2648,12 @@ void HCHostAssemble::ConstructJob(Compilation &C, const JobAction &JA,
   else
     Output.getInputArg().renderAsInput(Args, CmdArgs);
 
+  // hot-fix to support -fPIC for host objects in HC mode
+  bool IsPIC = Args.hasArg(options::OPT_fPIC);
+  if (IsPIC) {
+    CmdArgs.push_back("-fPIC");
+  }
+
   const char *Exec = getToolChain().getDriver().getHCHostAssembleProgramPath();
 
   C.addCommand(new Command(JA, *this, Exec, CmdArgs));
