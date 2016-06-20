a5428dcfeb479d7094017f9ab9c45d468aebb724
Jack Chung <whchung@gmail.com>
2016-06-20 12:01:11 +0800

    [Driver][Basic] revise -amdgpu-target command-line option for HCC
    Syntax:
    -amdgpu-target=(AMDGPU ISA version string)
    
    Semantic:
    - Optional. Fallback to ISA version string specified at CMake time if not specified.
    - Possible option for ISA version string:
      - AMD:AMDGPU:7:0:0 : maps to kaveri in AMDGPU backend
      - AMD:AMDGPU:7:0:1 : maps to hawaii in AMDGPU backend
      - AMD:AMDGPU:8:0:1 : maps to carrizo in AMDGPU backend
      - AMD:AMDGPU:8:0:3 : maps to fiji in AMDGPU backend
    - Single choice as of now.
    
    - Existing options of specifying via GPU family is still retained for backward compatibility.
    - Polaris GPUs have the same ISA version string as Fiji (AMD:AMDGPU:8:0:3).

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -7968,13 +7968,30 @@ void gnutools::CXXAMPLink::ConstructJob(Compilation &C, const JobAction &JA,
     assert(AMDGPUTargetArg->getNumValues() == 1);
     // check if valid AMDGPU target is specified
     StringRef AMDGPUTarget(AMDGPUTargetArg->getValue(0));
-    if (AMDGPUTarget.equals("fiji") ||
-        AMDGPUTarget.equals("kaveri") ||
-        AMDGPUTarget.equals("carrizo") ||
-        AMDGPUTarget.equals("hawaii")) {
-      Twine LinkerArgTwine = Twine("--amdgpu-target=") + AMDGPUTarget;
-      SmallString<32> LinkerArgString;
-      LinkerArgTwine.toVector(LinkerArgString);
+
+    bool FoundAMDGPUTarget = true;
+    SmallString<32> LinkerArgString("--amdgpu-target=");
+
+    // map ISA version string to GPU family
+    if (AMDGPUTarget.equals("AMD:AMDGPU:7:0:0")) {
+      LinkerArgString.append("kaveri");
+    } else if (AMDGPUTarget.equals("AMD:AMDGPU:7:0:1")) {
+      LinkerArgString.append("hawaii");
+    } else if (AMDGPUTarget.equals("AMD:AMDGPU:8:0:1")) {
+      LinkerArgString.append("carrizo");
+    } else if (AMDGPUTarget.equals("AMD:AMDGPU:8:0:3")) {
+      LinkerArgString.append("fiji");
+    } else if (AMDGPUTarget.equals("fiji") ||
+               AMDGPUTarget.equals("kaveri") ||
+               AMDGPUTarget.equals("carrizo") ||
+               AMDGPUTarget.equals("hawaii")) {
+      // directly use GPU family
+      LinkerArgString.append(AMDGPUTarget);
+    } else {
+      FoundAMDGPUTarget = false;
+    }
+
+    if (FoundAMDGPUTarget) {
       CmdArgs.push_back(Args.MakeArgString(LinkerArgString));
     } else {
       // ignore invalid AMDGPU target, use auto
